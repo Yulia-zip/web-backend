5c2590838209ceb9d6b3184021fd50f962e3a4e1
Yulia <yuliamaterikina@yande.ru>
2025-03-12 11:34:17 +0300

    3base

--- 3/base.sql
+++ 3/base.sql
@@ -1,25 +1,25 @@
-CREATE TABLE form(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-  name_fio VARCHAR(150) NOT NULL,
-  phone VARCHAR(15) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-	date_r DATE NOT NULL,
-	gender ENUM('male', 'female') NOT NULL,
-	biograf text NOT NULL,
-	contract_accepted TINYINT(1) NOT NULL,
+CREATE TABLE form (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name_fio VARCHAR(150) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    date_r DATE NOT NULL,
+    gender ENUM('male', 'female') NOT NULL,
+    biograf TEXT NOT NULL,
+    contract_accepted TINYINT(1) NOT NULL
 );
 
-CREATE TABLE lang(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name_lang VARCHAR(50) NOT NULL UNIQUE
+CREATE TABLE lang (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name_lang VARCHAR(50) NOT NULL UNIQUE
 );
 
-CREATE TABLE lang_check(
-	check_id INT(10) unsigned NOT NULL,
-	language_id INT(10) UNSIGNED NOT NULL,
-	PRIMARY KEY (check_id,language_id),
-	FOREIGN KEY check_id REFERENCES form(id),
-	FOREIGN KEY language_id REFERENCES lang(id_lang)
+CREATE TABLE lang_check (
+    check_id INT(10) UNSIGNED NOT NULL,
+    language_id INT(10) UNSIGNED NOT NULL,
+    PRIMARY KEY (check_id, language_id),
+    FOREIGN KEY (check_id) REFERENCES form(id),
+    FOREIGN KEY (language_id) REFERENCES lang(id)
 );
 
 INSERT INTO lang (name_lang) VALUES
