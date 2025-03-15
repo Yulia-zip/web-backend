1d1adefc2a76fae4d4eb11cb27a0142a5aeb6a2b
Yulia <yuliamaterikina@yande.ru>
2025-03-15 17:45:27 +0300

    php

--- 3/index.php
+++ 3/index.php
@@ -1,6 +1,13 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-include('form.php');
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (!empty($_GET['save'])) {
+
+      print('Спасибо, результаты сохранены.');
+    }
+    include('form.php');
+    exit();
+  }
 $errors = FALSE;
 $errorsMessages=[];
 $successMessage = '';
