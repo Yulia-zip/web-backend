ad9cad5ff9a592034359a745a21ea48b693103ba
Yulia <yuliamaterikina@yande.ru>
2025-03-15 17:26:01 +0300

    phpscript

--- 3/index.php
+++ 3/index.php
@@ -1,14 +1,5 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-
-  if (!empty($_GET['save'])) {
-    print('Спасибо, результаты сохранены.');
-  }
-  include('form.php');
-  exit();
-}
-
 $errors = FALSE;
 $errorsMessages=[];
 
