d78c32674ef81e908f3fa1e88dcf7aa61cd5f0d9
Yulia <yuliamaterikina@yande.ru>
2025-05-22 00:20:34 +0300

    7

--- 7/index.php
+++ 7/index.php
@@ -2,7 +2,6 @@
 error_reporting(0);
 ini_set('display_errors', 0);
 ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 1); 
 ini_set('session.use_strict_mode', 1);
 
 header("X-Content-Type-Options: nosniff");
