c06d20f55ef43eb36d64d95b3f7d372fc91bf25f
Yulia <yuliamaterikina@yande.ru>
2025-06-01 20:18:53 +0300

    rrr

---
+++ 8/project/modules/logout.php
@@ -0,0 +1,27 @@
+<?php
+require_once __DIR__ . '/../scripts/db.php';
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$_SESSION = [];
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+session_destroy();
+
+// Перенаправление на главную
+header('Location: /backend-s0188267.github.io/project/');
+exit;
